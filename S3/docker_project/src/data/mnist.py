c12685f994075982ad0364227db89ec25cc7c393
marcusroberto02 <marcusrobertonielsen02@gmail.com>
2023-01-04 16:35:27 +0100

    exercises for day 3

---
+++ S3/docker_project/src/data/mnist.py
@@ -0,0 +1,14 @@
+import torch
+import os
+
+def mnist():
+    print(os.getcwd())
+    Xtrain = torch.load("data/processed/Xtrain.pt")
+    Ytrain = torch.load("data/processed/Ytrain.pt")
+    Xtest = torch.load("data/processed/Xtest.pt")
+    Ytest = torch.load("data/processed/Ytest.pt")
+
+    trainset = torch.utils.data.TensorDataset(Xtrain,Ytrain)
+    testset = torch.utils.data.TensorDataset(Xtest,Ytest)
+
+    return trainset, testset
